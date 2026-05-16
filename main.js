3cfcbbaef6f0bc587bae39a72b94afdbcf0629dc
Marwan-Mostafa <marwan.mf.tech@gmail.com>
2026-05-16 21:15:17 +0300

    make the project dynamic

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const btn = document.getElementById("btn")
 const menu = document.getElementById("menu")
+let icon = document.querySelector(".fa-solid")
 
-btn.addEventListener("click", ()=>{
-    menu.classList.toggle("active") 
+
+btn.addEventListener("click", function (){
+    menu.classList.toggle("active")
+    icon.classList.toggle("fa-bars")
+    icon.classList.toggle("fa-xmark")
 })
