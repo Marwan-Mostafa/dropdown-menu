b0240a45f76d46c97e0f1bd59ac80454da857466
Marwan-Mostafa <marwan.mf.tech@gmail.com>
2026-05-17 11:21:54 +0300

    practice another dropdown

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-const btn = document.getElementById("btn")
-const menu = document.getElementById("menu")
-let icon = document.querySelector(".fa-solid")
+// const btn = document.getElementById("btn")
+// const menu = document.getElementById("menu")
+// let icon = document.querySelector(".fa-solid")
 
 
-btn.addEventListener("click", function (){
-    menu.classList.toggle("active")
-    icon.classList.toggle("fa-bars")
-    icon.classList.toggle("fa-xmark")
+// btn.addEventListener("click", function (){
+//     menu.classList.toggle("active")
+//     icon.classList.toggle("fa-bars")
+//     icon.classList.toggle("fa-xmark")
+// })
+
+
+/* Dropmenu */
+
+const link_drop = document.querySelector(".link-drop")
+const sub_menu = document.querySelector(".sub-menu")
+
+link_drop.addEventListener("click", function(){
+    sub_menu.classList.toggle('show')
+    link_drop.classList.toggle('active')
 })
